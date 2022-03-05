97313a0b4ed7a7eabb5beee02adb2d6cb7426712
João Maroeli <maroelijoao@gmail.com>
2022-03-05 14:21:01 -0300

    feat: Generate authentication user token route

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password are incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password are incorrect");
+    }
+
+    const token = sign({}, "afaaa1592aa029ccf4c0cfc6b2a1e1ba", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      token
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
